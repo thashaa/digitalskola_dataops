9d9c325dd1df754f5db2e2c8e8113df870835408
thashaa <107286289+thashaa@users.noreply.github.com>
2022-08-07 21:16:27 +0700

    Add files via upload

---
+++ dags/ingest/thasha/ingest_order_details.py
@@ -0,0 +1,55 @@
+import psycopg2
+import csv
+import sys
+from airflow.models import Variable
+
+psql_host = Variable.get("psql_host")
+psql_port = int(Variable.get("psql_port"))
+psql_user = Variable.get("psql_user")
+psql_password = Variable.get("psql_password")
+psql_db = Variable.get("psql_db")
+
+conn = None
+sql = """select * from order_details where order_id in (select order_id from orders where order_date='"""+sys.argv[1]+"""')"""
+csv_file_path = '/root/output/thasha/order_details/order_details'+sys.argv[1]+'.csv'
+
+try:
+    #connection to PostgreSQL
+    conn = psycopg2.connect(
+        user=psql_user,
+        password=psql_password,
+        dbname=psql_db,
+        host=psql_host,
+        port=psql_port
+    )
+
+    #run PostgreSQL query
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+finally:
+    if conn:
+        conn.close()
+
+# Continue only if there are rows returned.
+if rows:
+    # New empty list called 'result'. This will be written to a file.
+    result = list()
+
+    # The row name is the first entry for each entity in the description tuple.
+    column_names = list()
+    for i in cursor.description:
+        column_names.append(i[0])
+
+    result.append(column_names)
+    for row in rows:
+        result.append(row)
+
+    # Write result to file.
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in result:
+            csvwriter.writerow(row)
+else:
+    print("No rows found for query: {}".format(sql))
